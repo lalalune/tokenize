2e791e8bc0ad72d5e12e2aa8444f3ac09665005f
Spencer Brower <brower.spencer@gmail.com>
2022-02-28 15:42:45 -0500

    feat: tokenize can now accept input on STDIN.

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 #!/usr/bin/env node
-const { encode, decode } = require('gpt-3-encoder')
 
-const decoded = encode(process.argv[2]).map((token) => decode([token]))
+// Imports
+const { readFileSync } = require("fs");
+const { encode, decode } = require("gpt-3-encoder");
+const R = ({ compose, map, of, prop } = require("ramda"));
+const readline = require("readline");
 
-console.log(JSON.stringify(decoded))
+// decoded :: String -> String[]
+const decoded = (text) => R.map(R.compose(decode, R.of), encode(text));
+
+// decodedJSON :: String -> String
+const decodedJSON = R.compose(JSON.stringify, decoded);
+
+// readFromStdinStream :: String
+const readFromStdinStream = () => readFileSync(0).toString();
+
+// output :: String -> IO
+const output = compose(console.log, decodedJSON);
+
+// readFromStdinTTY :: (String -> IO) -> void
+function readFromStdinTTY(output) {
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
+
+  rl.on("line", output);
+}
+
+// main :: IO
+(function main() {
+  const input = R.prop(2, process.argv);
+
+  if (process.stdin.isTTY && !input) {
+    readFromStdinTTY(output);
+  } else {
+    output(input ?? readFromStdinStream());
+  }
+}) ();
